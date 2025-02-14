5beca0309bcb45d79a6b17f1ce81254dddc6a847
YolanFery <yolan@codethefuture.be>
2025-02-14 10:29:12 +0100

    Quotes

--- openhexa/cli/cli.py
+++ openhexa/cli/cli.py
@@ -245,9 +245,10 @@ def propose_to_create_template_version(workspace, pipeline_version, yes):
         )
         try:
             template = create_pipeline_template_version(workspace, pipeline["id"], pipeline_version["id"], changelog)
+            template_code_url = urllib.parse.quote(template["code"])
             click.echo(
                 click.style(
-                    f"✅ New version '{template['currentVersion']['versionNumber']}' of the template '{template['name']}' created! You can view the new template version in OpenHEXA on {click.style(f'{settings.public_api_url}/workspaces/{workspace}/templates/{urllib.parse.quote(template["code"])}/versions', fg='bright_blue', underline=True)}",
+                    f"✅ New version '{template['currentVersion']['versionNumber']}' of the template '{template['name']}' created! You can view the new template version in OpenHEXA on {click.style(f'{settings.public_api_url}/workspaces/{workspace}/templates/{template_code_url}/versions', fg='bright_blue', underline=True)}",
                     fg="green",
                 )
             )
