dcd0142f126cfa0e53113577c95b862eb4eb4a8b
YolanFery <yolan@codethefuture.be>
2025-03-05 10:25:04 +0100

    Fix last tests

--- tests/test_pipeline.py
+++ tests/test_pipeline.py
@@ -24,7 +24,7 @@ def test_pipeline_run_valid_config():
     parameter_1 = Parameter("arg1", type=str)
     parameter_2 = Parameter("arg2", type=str, multiple=True)
     parameter_3 = Parameter("arg3", type=int, default=33)
-    pipeline = Pipeline("code", "pipeline", pipeline_func, [parameter_1, parameter_2, parameter_3])
+    pipeline = Pipeline("pipeline", pipeline_func, [parameter_1, parameter_2, parameter_3])
     pipeline.run({"arg1": "ab", "arg2": ["cd", "ef"]})
 
     assert pipeline.name == "pipeline"
@@ -35,7 +35,7 @@ def test_pipeline_run_invalid_config():
     """Verify thatinvalid configuration values raise an exception."""
     pipeline_func = Mock()
     parameter_1 = Parameter("arg1", type=str)
-    pipeline = Pipeline("code", "pipeline", pipeline_func, [parameter_1])
+    pipeline = Pipeline("pipeline", pipeline_func, [parameter_1])
     with pytest.raises(ParameterValueError):
         pipeline.run({"arg1": 3})
 
@@ -44,7 +44,7 @@ def test_pipeline_run_extra_config():
     """Verify that extra (unexpected) configuration values raise an exception."""
     pipeline_func = Mock()
     parameter_1 = Parameter("arg1", type=str)
-    pipeline = Pipeline("code", "pipeline", pipeline_func, [parameter_1])
+    pipeline = Pipeline("pipeline", pipeline_func, [parameter_1])
     with pytest.raises(ParameterValueError):
         pipeline.run({"arg1": "ok", "arg2": "extra"})
 
@@ -67,7 +67,7 @@ def test_pipeline_run_connection_dhis2_parameter_config(workspace):
         name="this is a test for connection parameter",
         type=DHIS2Connection,
     )
-    pipeline = Pipeline("code", "pipeline", pipeline_func, [parameter_1])
+    pipeline = Pipeline("pipeline", pipeline_func, [parameter_1])
 
     data = DHIS2Connection(url, username, password)
     with patch.object(workspace, "get_connection", return_value=data):
@@ -94,7 +94,7 @@ def test_pipeline_run_connection_iaso_parameter_config(workspace):
         name="this is a test for connection parameter",
         type=IASOConnection,
     )
-    pipeline = Pipeline("code", "pipeline", pipeline_func, [parameter_1])
+    pipeline = Pipeline("pipeline", pipeline_func, [parameter_1])
 
     data = IASOConnection(url, username, password)
     with patch.object(workspace, "get_connection", return_value=data):
@@ -120,7 +120,7 @@ def test_pipeline_run_connection_gcs_parameter_config(workspace):
         name="this is a test for connection parameter",
         type=GCSConnection,
     )
-    pipeline = Pipeline("code", "pipeline", pipeline_func, [parameter_1])
+    pipeline = Pipeline("pipeline", pipeline_func, [parameter_1])
 
     data = GCSConnection(service_account_key, bucket_name)
 
@@ -148,7 +148,7 @@ def test_pipeline_run_connection_s3_parameter_config(workspace):
         name="this is a test for connection parameter",
         type=S3Connection,
     )
-    pipeline = Pipeline("code", "pipeline", pipeline_func, [parameter_1])
+    pipeline = Pipeline("pipeline", pipeline_func, [parameter_1])
 
     data = S3Connection(access_key_id, secret_access_key, bucket_name)
     with patch.object(workspace, "get_connection", return_value=data):
@@ -180,7 +180,7 @@ def test_pipeline_run_connection_postgres_parameter_config(workspace):
         type=PostgreSQLConnection,
     )
 
-    pipeline = Pipeline("code", "pipeline", pipeline_func, [parameter_1])
+    pipeline = Pipeline("pipeline", pipeline_func, [parameter_1])
 
     data = PostgreSQLConnection(host, port, username, password, database_name)
     with patch.object(workspace, "get_connection", return_value=data):
